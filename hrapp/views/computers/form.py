191ce916e0341336eb88c608f301489f3c9672c4
iamkaleb <kalebdmoran@gmail.com>
2020-08-11 14:16:25 -0500

    Working through computer form checklist

---
+++ hrapp/views/computers/form.py
@@ -0,0 +1,10 @@
+import sqlite3
+from django.shortcuts import render
+# from hrapp.models import Computer
+
+def computer_form(request):
+    if request.method == 'GET':
+        template = 'computers/form.html'
+        context = {}
+
+        return render(request, template, context)
--- hrapp/views/computers/list.py
+++ hrapp/views/computers/list.py
@@ -1,5 +1,5 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import redirect, reverse, render
 from hrapp.models import Computer
 from ..connection import Connection
 
@@ -40,5 +40,22 @@ def computer_list(request):
             }
 
             return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute('''
+            INSERT INTO hrapp_computer
+            (
+                manufacturer, make, purchase_date   
+            )
+            VALUES (?, ?, ?)
+            ''',
+            (form_data['manufacturer'], form_data['make'], form_data['purchase_date']))
+
+        return redirect(reverse('hrapp:computers'))
 
             

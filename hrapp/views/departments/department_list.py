02b56f3af0a1ef3aa4eae51d07d06de9004998b6
David Everett <davideverett1989@gmail.com>
2020-08-11 22:01:48 -0500

    Displaying single department detail with list of employees. Needs some refactoring but it works.

--- hrapp/views/departments/department_list.py
+++ hrapp/views/departments/department_list.py
@@ -3,27 +3,27 @@ from django.shortcuts import render, redirect, reverse
 from hrapp.models import Department
 from hrapp.models import Employee
 from ..connection import Connection
+from ..helpers.create_department import create_department
 
-def create_department(cursor, row):
-    _row = sqlite3.Row(cursor, row)
+# def create_department(cursor, row):
+#     _row = sqlite3.Row(cursor, row)
 
-    department = Department()
-    department.id = _row["id"]
-    department.name = _row["name"]
-    department.budget = _row["budget"]
+#     department = Department()
+#     department.id = _row["id"]
+#     department.name = _row["name"]
+#     department.budget = _row["budget"]
 
-    department.employees = list()
+#     department.employees = list()
 
-    employee = Employee()
-    employee.id = _row["employee_id"]
-    employee.first_name = _row["first_name"]
-    employee.last_name = _row["last_name"]
-    employee.start_date = _row["start_date"]
-    employee.is_supervisor = _row["is_supervisor"]
-    employee.department_id = _row["department_id"]
+#     employee = Employee()
+#     employee.id = _row["employee_id"]
+#     employee.first_name = _row["first_name"]
+#     employee.last_name = _row["last_name"]
+#     employee.start_date = _row["start_date"]
+#     employee.is_supervisor = _row["is_supervisor"]
+#     employee.department_id = _row["department_id"]
 
-    return (department, employee,)
-
+#     return (department, employee,)
 
 def department_list(request):
     if request.method == "GET":
@@ -45,8 +45,10 @@ def department_list(request):
 
                 if department.id not in department_dict:
                     department_dict[department.id] = department
-                    department_dict[department.id].employees.append(employee)
                     department_dict[department.id].num_of_employees = 0
+                    if not employee.id == None:
+                        department_dict[department.id].employees.append(employee)
+                        department_dict[department.id].num_of_employees = len(department_dict[department.id].employees)                
                 else:
                     department_dict[department.id].employees.append(employee)
                     department_dict[department.id].num_of_employees = len(department_dict[department.id].employees)

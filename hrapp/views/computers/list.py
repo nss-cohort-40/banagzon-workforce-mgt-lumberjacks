8fb725e4f6f91ee1488ea8e77920c968b5be1988
David Everett <davideverett1989@gmail.com>
2020-08-12 13:35:19 -0500

    User is able to filter computers by manufacturer and make.

--- hrapp/views/computers/list.py
+++ hrapp/views/computers/list.py
@@ -3,6 +3,70 @@ from django.shortcuts import redirect, reverse, render
 from hrapp.models import Computer
 from ..connection import Connection
 
+def computer_list_by_make(search_keyword):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.manufacturer,
+            c.make,
+            c.purchase_date,
+            c.decommission_date
+        FROM hrapp_computer c
+        WHERE c.make = ?
+        """, (search_keyword,))
+
+        all_computers = list()
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            computer = Computer()
+            computer.id = row['id']
+            computer.manufacturer = row['manufacturer']
+            computer.make = row['make']
+            computer.purchase_date = row['purchase_date']
+            computer.decommission_date = row['decommission_date']
+
+            all_computers.append(computer)
+
+        return all_computers
+
+def computer_list_by_manufacturer(search_keyword):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.manufacturer,
+            c.make,
+            c.purchase_date,
+            c.decommission_date
+        FROM hrapp_computer c
+        WHERE c.manufacturer = ?
+        """, (search_keyword,))
+
+        all_computers = list()
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            computer = Computer()
+            computer.id = row['id']
+            computer.manufacturer = row['manufacturer']
+            computer.make = row['make']
+            computer.purchase_date = row['purchase_date']
+            computer.decommission_date = row['decommission_date']
+
+            all_computers.append(computer)
+
+        return all_computers
+
 def computer_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
@@ -42,18 +106,42 @@ def computer_list(request):
     elif request.method == 'POST':
         form_data = request.POST
 
-        with sqlite3.connect(Connection.db_path) as conn:
-            db_cursor = conn.cursor()
+        if ("actual_method" in form_data and form_data["actual_method"] == "filter_by_make"):
+            search_keyword = form_data["make"]
+            all_computers = computer_list_by_make(search_keyword)
 
-            db_cursor.execute('''
-            INSERT INTO hrapp_computer
-            (
-                manufacturer, make, purchase_date   
-            )
-            VALUES (?, ?, ?)
-            ''',
-            (form_data['manufacturer'], form_data['make'], form_data['purchase_date']))
+            template = 'computers/list.html'
 
-        return redirect(reverse('hrapp:computer_list'))
+            context = {
+                'all_computers': all_computers
+            }
+
+            return render(request, template, context)
+
+        elif ("actual_method" in form_data and form_data["actual_method"] == "filter_by_manufacturer"):
+            search_keyword = form_data["manufacturer"]
+            all_computers = computer_list_by_manufacturer(search_keyword)
+
+            template = 'computers/list.html'
+
+            context = {
+                'all_computers': all_computers
+            }
+
+            return render(request, template, context)
+        else:
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute('''
+                INSERT INTO hrapp_computer
+                (
+                    manufacturer, make, purchase_date   
+                )
+                VALUES (?, ?, ?)
+                ''',
+                (form_data['manufacturer'], form_data['make'], form_data['purchase_date']))
+
+            return redirect(reverse('hrapp:computer_list'))
 
             

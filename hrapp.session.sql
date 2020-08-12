0911408f90a014af50dc37d55684ad653c13458b
iamkaleb <kalebdmoran@gmail.com>
2020-08-12 13:45:25 -0500

    Working through assigning employee

--- hrapp.session.sql
+++ hrapp.session.sql
@@ -1,4 +1,1 @@
-SELECT d.id, d.name, d.budget, e.id employee_id, e.first_name, e.last_name, e.start_date, e.is_supervisor, e.department_id
-FROM hrapp_department d
-LEFT JOIN hrapp_employee e ON e.department_id = d.id
-WHERE d.id = 1;
+
--- hrapp/views/computers/form.py
+++ hrapp/views/computers/form.py
@@ -1,10 +1,27 @@
 import sqlite3
 from django.shortcuts import render
-from hrapp.models import Computer
+from hrapp.models import Computer, Employee
+from ..connection import Connection
+
+def get_employees():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute('''
+        SELECT
+            e.id,
+            e.first_name,
+            e.last_name
+        ''')
+
+        return db_cursor.fetchall()
 
 def computer_form(request):
     if request.method == 'GET':
         template = 'computers/form.html'
-        context = {}
+        context = {
+            'all_computers': computers
+        }
 
         return render(request, template, context)

38c756809d1df3e9f05f3aa23a14abcea9c258cb
David Everett <davideverett1989@gmail.com>
2020-08-13 11:50:56 -0500

    wip

--- hrapp.session.sql
+++ hrapp.session.sql
@@ -11,3 +11,37 @@ SELECT
     c.decommission_date
 FROM hrapp_computer c
 WHERE c.manufacturer LIKE '%ap%';
+
+SELECT
+    c.id computer_id,
+    c.manufacturer,
+    c.make,
+    c.purchase_date,
+    c.decommission_date,
+    e.id employee_id,
+    e.first_name,
+    e.last_name,
+    e.start_date,
+    e.is_supervisor,
+    e.department_id
+FROM hrapp_computer c
+LEFT JOIN hrapp_employeecomputer ec ON ec.computer_id = c.id
+LEFT JOIN hrapp_employee e ON e.id = ec.employee_id
+WHERE c.id = 4;
+
+SELECT
+    c.id,
+    c.manufacturer,
+    c.make,
+    c.purchase_date,
+    c.decommission_date,
+    e.id employee_id,
+    e.first_name,
+    e.last_name,
+    e.start_date,
+    e.is_supervisor,
+    e.department_id
+FROM hrapp_computer c
+LEFT JOIN hrapp_employeecomputer ec ON ec.computer_id = c.id
+LEFT JOIN hrapp_employee e ON e.id = ec.employee_id
+WHERE c.id = 1;
